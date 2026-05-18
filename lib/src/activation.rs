bd15ac28b4677cb5223681723f7d831682fa8b30
adrianmikoda <adrian.mikodaa@gmail.com>
2026-05-18 16:04:08 +0200

    feat(lib): add activation implementation

--- lib/src/activation.rs
+++ lib/src/activation.rs
@@ -11,10 +11,36 @@ pub enum Activation {
 
 impl Activation {
     pub(crate) fn apply(&self, matrix: &mut Matrix) {
-        todo!()
+        match self {
+            Activation::ReLU => {
+                matrix.map(|x| if x > 0.0 { x } else { 0.0 });
+            }
+            Activation::Sigmoid => {
+                matrix.map(|x| 1.0 / (1.0 + (-x).exp()));
+            }
+            Activation::Softmax => {
+                let max = matrix.data.iter().cloned().fold(f32::NEG_INFINITY, f32::max);
+                matrix.map(|x| (x - max).exp());
+                let sum: f32 = matrix.data.iter().sum();
+                matrix.map(|x| x / sum);
+            }
+        }
     }
 
     pub(crate) fn apply_derivative(&self, matrix: &mut Matrix) {
-        todo!()
+        match self {
+            Activation::ReLU => {
+                matrix.map(|x| if x > 0.0 { 1.0 } else { 0.0 });
+            }
+            Activation::Sigmoid => {
+                matrix.map(|x| {
+                    let s = 1.0 / (1.0 + (-x).exp());
+                    s * (1.0 - s)
+                });
+            }
+            Activation::Softmax => {
+                let _ = matrix;
+            }
+        }
     }
 }
